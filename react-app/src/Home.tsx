5e85923489524d42dece2b6fac0f83ca54d2bd88
Andre Albarado <analbarado1@gmail.com>
2025-06-10 19:41:43 -0500

    Initial Commit

---
+++ react-app/src/Home.tsx
@@ -0,0 +1,17 @@
+type HomeProps = {
+  name: string;
+  title: string;
+  bio?: string;
+};
+
+function Home({ name, title, bio }: HomeProps) {
+  return (
+    <section className="home">
+      <h1>{name}</h1>
+      <h2>{title}</h2>
+      {bio && <p>{bio}</p>}
+    </section>
+  );
+}
+
+export default Home;
