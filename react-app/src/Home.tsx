04e545897f4078739756d460f87f60d13205f009
Andre Albarado <analbarado1@gmail.com>
2025-06-16 20:56:00 -0500

    Tailwind Cards

--- react-app/src/Home.tsx
+++ react-app/src/Home.tsx
@@ -8,12 +8,14 @@ function Home({ name, title, bio }: HomeProps) {
   return (
     <section
       className="flex h-screen"
-      style={{ backgroundColor: "oklch(78.5% 0.115 274.713)" }}
+      style={{ backgroundColor: "oklch(26.9% 0 0)" }}
     >
-      <div className="w-1/2">Left Side Content</div>
+      <div id="leftCard" className="w-1/2 bg-neutral-200 rounded-md m-6 p-2">
+        Left Side Content
+      </div>
 
       <div className="w-1/2 flex flex-col items-end p-8 text-right font-serif">
-        <div className="bg-grid-pattern p-6 rounded-md">
+        <div className="bg-grid-pattern bg-neutral-200 rounded-md m-6 p-2">
           <h1 className="text-4xl font-bold">{name}</h1>
           <h2 className="text-2xl text-gray-600">{title}</h2>
           <p className="text-base mt-4 max-w-md">{bio}</p>
